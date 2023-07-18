2d7743e0b50c76e69400020e639069da96ed974a
Allanpt <jatallantavares@gmail.com>
2023-07-18 08:16:14 -0300

    DepartmentDaoJDBC and Program_department complete

--- src/app/Program_department.java
+++ src/app/Program_department.java
@@ -4,6 +4,7 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Program_department {
@@ -14,21 +15,59 @@ public class Program_department {
         DepartmentDao departmentDao = DaoFactory.creatDepartmentDao();
 
         System.out.println("=== TEST 1 - Insert Department");
-        Department dep = new Department(null,"Games");
+        System.out.println("What's the new Department do you want to insert? ");
+        String insertNewDepartment = sc.nextLine();
+        Department dep = new Department(null,insertNewDepartment);
         departmentDao.insert(dep);
 
         System.out.println();
 
         System.out.println("=== TEST 2 - Delete Department");
-        System.out.println("How many department do you want to delete?");
+        System.out.print("How many department do you want to delete? ");
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            System.out.println("Which department ID do you want to delete?: ");
-
+            List<Department> list = departmentDao.findAll();
+            for(Department d : list){
+                System.out.println(d);
+            }
+            System.out.print("Which department ID do you want to delete?: ");
             int depId = sc.nextInt();
             departmentDao.deleteById(depId);
         }
 
+        System.out.println();
+
+        System.out.println("=== TEST 3 - FindAll");
+        List<Department> list = departmentDao.findAll();
+        for(Department d : list){
+            System.out.println(d);
+        }
+
+        System.out.println();
+
+        System.out.println("=== TEST 4 - FindById");
+        for(Department d : list){
+            System.out.println(d);
+        }
+        System.out.print("Which department ID do you want to find? ");
+        int department = sc.nextInt();
+        Department depId = departmentDao.findById(department);
+        System.out.println(depId);
+
+        System.out.println();
+
+        System.out.println("=== TEST 5 - Update");
+        for(Department d : list){
+            System.out.println(d);
+        }
+        System.out.println("Which department ID do you want to update? ");
+        int depIdUpdate = sc.nextInt();
+        Department depUpdate = departmentDao.findById(depIdUpdate);
+        System.out.println("What's the new 'Name' department");
+        sc.nextLine();
+        String newDepName = sc.nextLine();
+        depUpdate.setName(newDepName);
+        departmentDao.update(depUpdate);
 
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -6,6 +6,7 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
@@ -77,6 +78,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void deleteById(Integer id) {
+
         PreparedStatement st = null;
         try {
             st = conn.prepareStatement(
@@ -96,11 +98,66 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public Department findById(Integer id) {
-        return null;
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT * FROM department WHERE Id = ?");
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()){
+
+                Department dep = instatiateDepartment(rs);
+                return dep;
+            }
+            return null;
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
     public List<Department> findAll() {
-        return null;
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT * FROM department order by Id");
+
+            rs = st.executeQuery();
+
+            List<Department> list = new ArrayList<>();
+
+            while (rs.next()){
+
+                Department dep = instatiateDepartment(rs);
+                list.add(dep);
+            }
+            return list;
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+    private Department instatiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("Id"));
+        dep.setName(rs.getString("Name"));
+        return dep;
     }
 }
